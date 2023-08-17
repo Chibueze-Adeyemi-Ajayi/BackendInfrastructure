9a37c9d9a37f6aec0c89fb8fad18f2fb2d25add6
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-08-17 14:40:27 +0000

    ProxyServer

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
 const exports_1 = require("./src/config/exports");
 const express_1 = __importDefault(require("express"));
 const helper_1 = require("./src/helper");
 const app_1 = require("./src/app");
 const cron_job_1 = require("./src/services/cron-job");
+const config_1 = require("./src/config");
 // using the enviromental port
 const port = exports_1.PORT, app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: true }));
@@ -17,5 +19,6 @@ application.mountRoutes(app);
 // mounting cron jobs
 const cronController = new cron_job_1.CronController();
 cronController.systemLogger(); // every one minute system logger
-// configuring the application 
+const config = config_1.CONFIGURATION_STATE.DEVELOPMENT; //you can set it to production or preproduction 
+exports.config = config;
 app.listen(port, () => (0, helper_1.info)(`Application listening on:`, { port }, { BASE_URL: exports_1.BASE_URL }));
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { info } from "./src/helper";
 import { Application } from "./src/app";
 import { CronController } from "./src/services/cron-job";
+import { CONFIGURATION_STATE } from "./src/config";
  
 // using the enviromental port
 const port = PORT,
@@ -17,7 +18,9 @@ application.mountRoutes(app);
 const cronController = new CronController();
 cronController.systemLogger(); // every one minute system logger
 
-// configuring the application 
-
+const config = CONFIGURATION_STATE.DEVELOPMENT; //you can set it to production or preproduction 
 
 app.listen(port, () => info(`Application listening on:`, {port}, {BASE_URL}));
+
+// configuring the application 
+export {config};
